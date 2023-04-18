3c6ded4de02730e61f75845cb29f906d95a45649
AbelSyx <876688075@qq.com>
2023-04-18 22:18:24 +0800

    add noise to odom and test locate error influence

--- scripts/utilities/add_noise2odom_huai.py
+++ scripts/utilities/add_noise2odom_huai.py
@@ -6,50 +6,85 @@ import numpy as np
 from nav_msgs.msg import Odometry
 import tf
 
-noisy_std = 0.1
+pos_noisy_std = 0.2
+theta_noisy_std = 0.01
 
+last_odom = None
 new_odom_topic = ""
 odom_topic = ""
  
 def callback(odom_data):
     
-    truth_odom = Odometry()
+    global last_odom
+    noisy_odom = Odometry()
+    
+    '''
+    q2 = [ odom_data.pose.pose.orientation.x,
+           odom_data.pose.pose.orientation.y,
+           odom_data.pose.pose.orientation.z,
+           odom_data.pose.pose.orientation.w ]
+    (r2, p2, theta2) = tf.transformations.euler_from_quaternion(q2)
+    '''
+    
+    if last_odom == None:
+        
+        last_odom = odom_data
+        noisy_odom = odom_data
  
-    truth_odom.header = odom_data.header
+    else:
+        
+        dx = odom_data.pose.pose.position.x - last_odom.pose.pose.position.x
+        dy = odom_data.pose.pose.position.y - last_odom.pose.pose.position.y
+        ds = sqrt(dx*dx + dy*dy)
+        if ds > 6:
+            last_odom = odom_data
+            noisy_odom = odom_data
+            
+        else:
+            dq_x = odom_data.pose.pose.orientation.x - last_odom.pose.pose.orientation.x
+            dq_y = odom_data.pose.pose.orientation.y - last_odom.pose.pose.orientation.y
+            dq_z = odom_data.pose.pose.orientation.z - last_odom.pose.pose.orientation.z
+            dq_w = odom_data.pose.pose.orientation.w - last_odom.pose.pose.orientation.w
+        
+            '''
+            q1 = [ last_odom.pose.pose.orientation.x,
+                   last_odom.pose.pose.orientation.y,
+                   last_odom.pose.pose.orientation.z,
+                   last_odom.pose.pose.orientation.w ]
+            (r1,p1, theta1) = tf.transformations.euler_from_quaternion(q1)
+        
+            q = [ odom_data.pose.pose.orientation.x,
+                  odom_data.pose.pose.orientation.y,
+                  odom_data.pose.pose.orientation.z,
+                  odom_data.pose.pose.orientation.w ]
+            (r,p, theta) = tf.transformations.euler_from_quaternion(q)
+            '''
+        
+            noisy_odom.header = odom_data.header
+            # Add noisy to x, y, z position
+            noisy_odom.pose.pose.position.x += dx * (1 + pos_noisy_std)
+            noisy_odom.pose.pose.position.y += dy * (1 + pos_noisy_std)
+            noisy_odom.pose.pose.position.z = odom_data.pose.pose.position.z
     
-    truth_odom.pose.pose.position.x = odom_data.pose.pose.position.x
-    truth_odom.pose.pose.position.y = odom_data.pose.pose.position.y
-    truth_odom.pose.pose.position.z = odom_data.pose.pose.position.z
-    truth_odom.pose.pose.orientation.x = odom_data.pose.pose.orientation.x 
-    truth_odom.pose.pose.orientation.y = odom_data.pose.pose.orientation.y
-    truth_odom.pose.pose.orientation.z = odom_data.pose.pose.orientation.z
-    truth_odom.pose.pose.orientation.w = odom_data.pose.pose.orientation.w
-    truth_odom.twist = odom_data.twist
+            # Add noisy to orientation
+            #dtheta = theta2 - theta1
+            #theta += dtheta * (1 + theta_noisy_std)
+        
+            #noisy_q = tf.transformations.quaternion_from_euler(r2, p2, theta)
+            noisy_odom.pose.pose.orientation.x = odom_data.pose.pose.orientation.x + random.gauss(0, theta_noisy_std)
+            noisy_odom.pose.pose.orientation.y = odom_data.pose.pose.orientation.y + random.gauss(0, theta_noisy_std)
+            noisy_odom.pose.pose.orientation.z = odom_data.pose.pose.orientation.z + random.gauss(0, theta_noisy_std)
+            noisy_odom.pose.pose.orientation.w = odom_data.pose.pose.orientation.w + random.gauss(0, theta_noisy_std)
+        
+            '''
+            noisy_odom.pose.pose.orientation.x += dq_x * (1 + theta_noisy_std)
+            noisy_odom.pose.pose.orientation.y += dq_y * (1 + theta_noisy_std)
+            noisy_odom.pose.pose.orientation.z += dq_z * (1 + theta_noisy_std)
+            noisy_odom.pose.pose.orientation.w += dq_w * (1 + theta_noisy_std)
+            '''
+            noisy_odom.twist = odom_data.twist
     
-    # Add noisy to x, y, z position
-    odom_data.pose.pose.position.x = odom_data.pose.pose.position.x + random.gauss(0, noisy_std)
-    odom_data.pose.pose.position.y = odom_data.pose.pose.position.y + random.gauss(0, noisy_std)
-    odom_data.pose.pose.position.z = odom_data.pose.pose.position.z + random.gauss(0, noisy_std/10)
-    
-    # Add noisy to orientation
-    odom_data.pose.pose.orientation.x = odom_data.pose.pose.orientation.x + random.gauss(0, noisy_std)
-    odom_data.pose.pose.orientation.y = odom_data.pose.pose.orientation.y + random.gauss(0, noisy_std)
-    odom_data.pose.pose.orientation.z = odom_data.pose.pose.orientation.z + random.gauss(0, noisy_std)
-    odom_data.pose.pose.orientation.w = odom_data.pose.pose.orientation.w + random.gauss(0, noisy_std)
-    
-    
-    pub.publish(truth_odom)
-    
-    '''
-    # publish tf
-    br = tf.TransformBroadcaster()
-    #print("-------------------I'm coming!----------------------")
-    br.sendTransform((pose[0] - odom_data.pose.pose.position.x, pose[1] - odom_data.pose.pose.position.y, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, pose[2] - theta2),
-                     odom_data.header.stamp,
-                     odom_frame,
-                     new_odom_frame)
-    '''
+    pub.publish(noisy_odom)
     
 
  
@@ -57,9 +92,15 @@ if __name__ == '__main__':
     rospy.init_node('noisy_odometry', anonymous=True)
     
     if rospy.has_param("~noisy_std"):
-        noisy_std = rospy.get_param("~noisy_std")
+        pos_noisy_std = rospy.get_param("~noisy_std")
     else:
-        noisy_std = 5
+        pos_noisy_std = 0.2
+        rospy.logwarn("noisy_std is set to default")
+        
+    if rospy.has_param("~noisy_std"):
+        theta_noisy_std = rospy.get_param("~noisy_std")
+    else:
+        theta_noisy_std = 0.01
         rospy.logwarn("noisy_std is set to default")
         
     # get odom topic
@@ -71,7 +112,7 @@ if __name__ == '__main__':
     if rospy.has_param("~new_odom_frame"):
         new_odom_topic = rospy.get_param("~new_odom_frame")
     else:
-        new_odom_topic = "/turtlebot3_0/odom_truth"
+        new_odom_topic = "/turtlebot3_0/odom_noisy"
 
     '''
     # get base frame
